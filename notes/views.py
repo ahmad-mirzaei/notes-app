20a2e7cf17c9b69ec0d0de765cd8d9de71a7dd98
aleph mirzaei <ahmadmirzaei.py@gmail.com>
2026-06-04 17:15:25 +0330

    refactor urls to app-level routing and use CBV for notes list

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Note
 # Create your views here.
+
+class NoteListView(ListView):
+    model = Note
+    template_name = 'notes/note_list.html'
+    context_object_name = 'notes'
