b4575e54c2174987fd2b97a7ddd24a263cd1f133
aleph <ahmadmirzaei.py@gmail.com>
2026-06-27 09:59:47 +0330

    configure authentication redirect settings

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+LOGIN_URL = "login"
+
+LOGIN_REDIRECT_URL = "note-list"
+
+LOGOUT_REDIRECT_URL = "login"
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
